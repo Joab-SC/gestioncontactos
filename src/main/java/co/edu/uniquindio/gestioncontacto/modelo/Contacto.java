9cc35afdb23a6e3e3e1c7adc3b33bf271aeaaf71
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2025-03-21 07:24:48 -0500

    .

--- src/main/java/co/edu/uniquindio/gestioncontacto/modelo/Contacto.java
+++ src/main/java/co/edu/uniquindio/gestioncontacto/modelo/Contacto.java
@@ -2,7 +2,6 @@ package co.edu.uniquindio.gestioncontacto.modelo;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
-import lombok.experimental.Accessors;
 
 
 import java.time.LocalDate;
